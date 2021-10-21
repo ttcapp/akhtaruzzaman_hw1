55f800b7e90045e692d0a680c482bb6b05fc6db5
TTC <banglajoy701@gmail.com>
2021-10-21 04:46:37 -0700

    akhtaruzzaman_hw1

---
+++ android/app/src/main/kotlin/com/example/akhtaruzzaman_hw1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.akhtaruzzaman_hw1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("first app"),
+      ),
+
+    );
+  }
+}
